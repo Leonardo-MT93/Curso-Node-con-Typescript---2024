beb4dff3747b48b7502c8872b071bed386b9aba0
Leonardo Tolaba <leonardotolaba.20@gmail.com>
2024-06-24 19:53:40 -0300

    Uso de winston y creación de loggers

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -11,10 +11,15 @@
 // const johnDoe = makePerson (obj)
 // console.log(johnDoe)
 
-const getPokemonById = require('./js-foundation/06-promises')
+// const getPokemonById = require('./js-foundation/06-promises')
+const {buildLogger} = require('./plugins/')
 
-getPokemonById(6)
-    .then((pokemon) => console.log(pokemon))
-    .catch((error) => console.error(error))
-    .finally(() => console.log('Pokemon fetched!'));
+// getPokemonById(6)
+//     .then((pokemon) => console.log(pokemon))
+//     .catch((error) => console.error(error))
+//     .finally(() => console.log('Pokemon fetched!'));
 
+const logger = buildLogger('app.js'); //archivo que mando a llamar
+
+logger.log('Hola mundo');
+logger.error('Esto es un error')
